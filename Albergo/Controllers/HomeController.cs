e37ae5ca485719fe3b26ba0a87a8c39cd7c8ee87
MethaKinde <meethabeelkinde@gmail.com>
2024-03-09 12:54:31 +0100

    last modifiche

--- Albergo/Controllers/HomeController.cs
+++ Albergo/Controllers/HomeController.cs
@@ -1,14 +1,63 @@
-﻿using System;
+﻿using Albergo.Models;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace Albergo.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
+        {
+            if (HttpContext.User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("LoggedIn", "Home"); //sei già loggato, torna alla home
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult About(Dipendente dipendente)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["AlbergoDb"].ToString();
+            var conn = new SqlConnection(connectionString);
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    conn.Open();
+                    var command = new SqlCommand("SELECT * FROM Dipendenti WHERE Username = @username AND Pass = @password", conn);
+                    command.Parameters.AddWithValue("@username", dipendente.Username);
+                    command.Parameters.AddWithValue("@password", dipendente.Pass);
+                    var reader = command.ExecuteReader();
+
+                    if (reader.HasRows)
+                    {
+                        reader.Read();
+                        FormsAuthentication.SetAuthCookie(reader["IdDipendente"].ToString(), true);
+                        return RedirectToAction("LoggedIn", "Home");
+                    }
+                    else
+                    {
+                        return View("error");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return View("error");
+                }
+                finally { conn.Close(); }
+            }
+            return View("LoggedIn");
+        }
+
+        [Authorize]
+        public ActionResult LoggedIn()
         {
             return View();
         }
---
+++ Albergo/Controllers/ServiziController.cs
@@ -0,0 +1,73 @@
+﻿using Albergo.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Albergo.Controllers
+{
+    public class ServiziController : Controller
+    {
+        // GET: Servizi
+        public ActionResult NuovoServizio()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["AlbergoDb"].ToString();
+            var conn = new SqlConnection(connectionString);
+            List<Prenotazione> listaPrenotati = new List<Prenotazione>();
+            conn.Open();
+            var commandList = new SqlCommand("SELECT * FROM Prenotazioni", conn);
+            var readerList = commandList.ExecuteReader();
+
+            if (readerList.HasRows)
+            {
+                while (readerList.Read())
+                {
+                    var prenotazione = new Prenotazione()
+                    {
+                        IdPrenotazione = (int)readerList["IdPrenotazione"],
+                        Cognome = (string)readerList["Cognome"],
+                        Nome = (string)readerList["Nome"]
+                    };
+                    listaPrenotati.Add(prenotazione);
+                }
+                ViewBag.listaPrenotati = listaPrenotati;
+                conn.Close();
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult NuovoServizio(Servizio nuovoServizio)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["AlbergoDb"].ToString();
+            var conn = new SqlConnection(connectionString);
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    conn.Open();
+                    var command = new SqlCommand(@"INSERT INTO Servizi 
+                    (TipoServizio, DataServizio, Quantita, Prezzo, IdPrenotazione)
+                    VALUES (@tipoServizio, @dataServizio, @quantita, @prezzo, @idPrenotazione)", conn);
+                    command.Parameters.AddWithValue("@tipoServizio", nuovoServizio.TipoServizio);
+                    command.Parameters.AddWithValue("@dataServizio", nuovoServizio.DataServizio);
+                    command.Parameters.AddWithValue("@quantita", nuovoServizio.Quantita);
+                    command.Parameters.AddWithValue("@prezzo", nuovoServizio.Prezzo);
+                    command.Parameters.AddWithValue("@idPrenotazione", nuovoServizio.IdPrenotazione);
+                    var nRows = command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    return View("error");
+                }
+                finally { conn.Close(); }
+                return RedirectToAction("LoggedIn", "Home");
+            }
+            return View();
+
+        }
+    }
+}
